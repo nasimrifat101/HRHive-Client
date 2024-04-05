6d9981424227650baeab9bed1091621b2d862384
nasimrifat101 <nasimrifat101@gmail.com>
2024-04-05 12:08:45 +0600

    [Navbar] fixed blurring issue

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,12 +1,14 @@
 import Cursor from "../utils/Cursor";
 import Banner from "./homeponents/Banner";
 import Navbar from "./homeponents/Navbar";
+import Vision from "./homeponents/Vision";
 
 const Home = () => {
   return (
     <div className="relative">
       <Navbar />
       <Banner />
+      <Vision/>
       <div className="absolute">
         <Cursor />
       </div>
